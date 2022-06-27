45764d49d7af0bb5e7fdf10fb6e3daed2a3731a1
Tiagocsl <tiagocslll@outlook.com>
2022-06-27 17:49:00 -0300

    FEAT: Create Transaction files.
    Foi criado a entidade Transaction, e os casos de uso basicos ( Create,
    GetById, GetAll ).
    Foi criado também os arquivos de testes.
    Casos de uso criados utilizando TDD.

--- src/infra/middlewares/morgan.middleware.ts
+++ src/infra/middlewares/morgan.middleware.ts
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import morgan from "morgan";
-import Logger from "../utils/logger";
+import morgan from 'morgan';
+import Logger from '../../utils/logger';
 
 const stream = {
     write: (message: any) => Logger.http(message),
 };
 
 const skip = () => {
-    const env = process.env.NODE_ENV || "development";
-    return env != "development";
+    const env = process.env.NODE_ENV || 'development';
+    return env != 'development';
 };
 
 const morganMiddleware = morgan(
-    ":remote-addr :method :url :status :res[content-length] - :response-time ms",
+    ':remote-addr :method :url :status :res[content-length] - :response-time ms',
     { stream, skip }
 );
 
